7b8ed99b15422f1a56a871f041cbc48cdb7f3481
Jonatas Fontele <jonatasfontele@gmail.com>
2021-04-13 10:58:26 -0300

    Day 9 challenge

--- day_9_recursion_3.py
+++ day_9_recursion_3.py
@@ -6,14 +6,9 @@ def factorial(n):
 
 
 def main():
-    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
     n = int(input())
     if 2 <= n <= 12:
         print(factorial(n))
-        # result = factorial(n)
-
-    # fptr.write(str(result) + '\n')
-    # fptr.close()
 
 
 if __name__ == "__main__":
