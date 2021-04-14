bdb250a6ea9e933b1ad0f1de136a7fdab76853d5
Jonatas Fontele <jonatasfontele@gmail.com>
2021-04-14 11:03:38 -0300

    Day 9 challenge

--- day_9_recursion_3.py
+++ day_9_recursion_3.py
@@ -1,3 +1,6 @@
+import os
+
+
 def factorial(n):
     if n <= 1:
         return 1
@@ -6,9 +9,16 @@ def factorial(n):
 
 
 def main():
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # The above came in Hackerrank, write variables into your operating system as environment variables
+
     n = int(input())
     if 2 <= n <= 12:
         print(factorial(n))
+        # result = factorial(n)
+
+    # fptr.write(str(result) + '\n')
+    # fptr.close()
 
 
 if __name__ == "__main__":
