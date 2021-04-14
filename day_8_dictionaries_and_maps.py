d25a587aedf83d28344b7d8f4855a27d248fc4dd
Jonatas Fontele <jonatasfontele@gmail.com>
2021-04-14 11:02:56 -0300

    Day 8 challenge

--- day_8_dictionaries_and_maps.py
+++ day_8_dictionaries_and_maps.py
@@ -1,6 +1,7 @@
 def query(contacts):
     queries = 0
     name_query = input()
+    # Read unknown number of lines of queries (name)
     while name_query != "" and queries <= 10**5:
         try:
             found_number = contacts.get(name_query, "Not found")
@@ -15,11 +16,11 @@ def query(contacts):
 
 def main():
     contacts = {}
-    n = int(input())
+    n = int(input())  # Number of entries
     if 1 <= n <= 10**5:
         for i in range(n):
-            name_number = input().split()
-            contacts[name_number[0]] = name_number[1]
+            name_number = input().split()  # name number (per line)
+            contacts[name_number[0]] = name_number[1]  # {name: number}
         query(contacts)
 
 
