0d4a79c26e727ca790f9e4e64ac378464b014fce
Jonatas Fontele <jonatasfontele@gmail.com>
2021-04-28 02:02:51 -0300

    Day 14 challenge

--- day_14_scope.py
+++ day_14_scope.py
@@ -1,15 +1,25 @@
 class Difference:
     def __init__(self, a):
         self.__elements = a
+        self.maximumDifference = 0
 
-	# Add your code here
+    def computeDifference(self):
+        self.maximumDifference = abs(max(self.__elements) - min(self.__elements))
 
-# End of Difference class
+        # Nested and not recommended way
+        # for a in self.__elements:
+        #    for b in self.__elements:
+        #        if abs(a - b) > self.maximumDifference:
+        #            self.maximumDifference = abs(a - b)
 
-_ = input()
-a = [int(e) for e in input().split(' ')]
 
-d = Difference(a)
-d.computeDifference()
+def main():
+    _ = input()
+    a = [int(e) for e in input().split(' ')]
+    d = Difference(a)
+    d.computeDifference()
+    print(d.maximumDifference)
 
-print(d.maximumDifference)
+
+if __name__ == "__main__":
+    main()
