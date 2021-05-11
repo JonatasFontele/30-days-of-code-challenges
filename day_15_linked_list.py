b265a33c09e253a45e10cf59886aa83e06cf0ec3
Jonatas Fontele <jonatasfontele@gmail.com>
2021-05-10 23:32:42 -0300

    Day 15 challenge

--- day_15_linked_list.py
+++ day_15_linked_list.py
@@ -1,26 +1,36 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
-        self.next = None 
-class Solution: 
-    def display(self,head):
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
         current = head
         while current:
-            print(current.data,end=' ')
+            print(current.data, end=' ')
             current = current.next
 
-    def insert(self,head,data): 
-        pass
+    def insert(self, head, data):
+        new_node = Node(data)
+        if head is None:
+            head = new_node
+            return head
+        current = head
+        while current.next:
+            current = current.next
+        current.next = new_node
+        return head
 
 
 def main():
-    mylist= Solution()
-    T=int(input())
-    head=None
+    mylist = Solution()
+    T = int(input())
+    head = None
     for i in range(T):
-        data=int(input())
-        head = mylist.insert(head,data)
-    mylist.display(head);
+        data = int(input())
+        head = mylist.insert(head, data)
+    mylist.display(head)
 
 
 if __name__ == "__main__":
