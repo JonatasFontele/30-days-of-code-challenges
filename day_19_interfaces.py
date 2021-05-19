97d9a193f1862feac1d1a30920a13ec7f009d354
Jonatas Fontele <jonatasfontele@gmail.com>
2021-05-18 22:44:21 -0300

    Day 19 challenge
    If we look carefully, all the divisors are present in pairs. For example if n = 100, then the various pairs of divisors are: (1,100), (2,50), (4,25), (5,20), (10,10)
    Using this fact we could speed up our program significantly. 
    We, however, have to be careful if there are two equal divisors as in the case of (10, 10). In such case, we’d print only one of them.

---
+++ day_19_interfaces.py
@@ -0,0 +1,26 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum = 0
+        # O(sqrt(n)) instead of brute-force technique O(n)
+        for i in range(1, int(n ** 0.5) + 1):
+            if n % i == 0:
+                # If divisors are equal, print only one
+                if n / i == i:
+                    sum += i
+                # Otherwise print both
+                else:
+                    sum += i
+                    sum += int(n / i)
+        return sum
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
