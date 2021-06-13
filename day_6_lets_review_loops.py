24741eff777ea1144d4d8134bc7fcf8f76f89c3b
Jonatas Fontele <jonatasfontele@gmail.com>
2021-06-13 10:53:27 -0300

    Day 6 challenge

--- day_6_lets_review_loops.py
+++ day_6_lets_review_loops.py
@@ -4,11 +4,11 @@ def even_odd_index(s):
     odd = ""
     for i in range(len(s)):
         # string.index(value, start, end)
-        if s.index(s[i], i, len(s)) % 2 == 0:
-            even = even + s[i]
-        else:
+        if s.index(s[i], i, len(s)) % 2:
             odd = odd + s[i]
-    print(even + " " + odd)
+        else:
+            even = even + s[i]
+    print(f"{even} {odd}")
 
 
 def main():
