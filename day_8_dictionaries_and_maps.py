2e2c0940f434537a481a412e8aaac1b42701a403
Jonatas Fontele <jonatasfontele@gmail.com>
2021-06-18 23:24:05 -0300

    Day 8 challenge
    Dictionary Comprehension

--- day_8_dictionaries_and_maps.py
+++ day_8_dictionaries_and_maps.py
@@ -2,11 +2,11 @@ def query(contacts):
     queries = 0
     name_query = input()
     # Read unknown number of lines of queries (name)
-    while name_query != "" and queries <= 10**5:
+    while name_query != "" and queries <= 100000:
         try:
             found_number = contacts.get(name_query, "Not found")
             if found_number != "Not found":
-                print("%s=%s" % (name_query, found_number))
+                print(f"{name_query}={found_number}")
             else:
                 print(found_number)
             name_query = input()
@@ -15,12 +15,13 @@ def query(contacts):
 
 
 def main():
-    contacts = {}
     n = int(input())  # Number of entries
-    if 1 <= n <= 10**5:
-        for i in range(n):
-            name_number = input().split()  # name number (per line)
-            contacts[name_number[0]] = name_number[1]  # {name: number}
+    if 1 <= n <= 100000:
+        # for i in range(n):
+        #   name_number = input().split()               name number (per line)
+        #   contacts[name_number[0]] = name_number[1]   {name: number}
+
+        contacts = dict([input().split() for _ in range(n)])  # Pythonic way
         query(contacts)
 
 
